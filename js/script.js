82e85e72008be155970f862f0dd8d0e0b6f2e604
yokra <yordanka_krasteva@mail.bg>
2019-02-06 12:12:56 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const link = "​http://petlatkea.dk/2019/students1991.json";
+const template = document.querySelector("#Student-list").content;
+
+function getNames() {
+  fetch(link)
+    .then(res => res.json())
+    .then(showNames);
+}
+
+function showNames(nameList) {
+  console.log(nameList);
+  nameList.forEach(name => {
+    console.log(name.fullname);
+    const clone = template.cloneNode(true);
+
+    clone.querySelector("h1 span").content = name.fullname;
+    clone.querySelector("h2 span").content = name.house;
+
+    document.querySelector("main").appendChild(clone);
+  });
+}
+
+getNames();
